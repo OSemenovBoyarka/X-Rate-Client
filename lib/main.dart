6f9ee7911311b2554da764506ab7730e15611aac
Alexander Semenov <alexs@2kgroup.com>
2019-04-16 23:38:37 +0300

    Basic UI, connect data to UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:x_rate_monitor/data.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,8 +24,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController _baseAmountController;
+  CurrencyRate _currencyRate;
 
-  String _selectedCurrency;
+  @override
+  void initState() {
+    // TODO add async data init
+    _currencyRate = getRates();
+
+    _baseAmountController = TextEditingController(text: "1,00");
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,29 +42,60 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(),
       body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: DropdownButton<String>(
-                  hint: Text("Pick currency"),
-                  value: _selectedCurrency,
-                  items: ["USD", "EUR"]
-                      .map((currency) => DropdownMenuItem(
-                            value: currency,
-                            child: Text(currency),
-                          ))
-                      .toList(),
-                  onChanged: (String value) {
-                    setState(() {
-                      _selectedCurrency = value;
-                    });
-                    print("Currency selected: $value");
-                  },
-                ),
-              )
-            ],
+          Card(
+            child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    DropdownButton<String>(
+                      value: _currencyRate.baseCurrency,
+                      // TODO more items
+                      items: ["USD", "EUR"].map((currency) {
+                        return DropdownMenuItem(
+                          value: currency,
+                          child: Text(currency),
+                        );
+                      }).toList(),
+                      onChanged: (String value) {
+                        setState(() {
+                          // fetch new data from network
+                          // TODO cancel previous downloads
+                          _currencyRate = getRates(baseCurrency: value);
+                        });
+                        print("Currency selected: $value");
+                      },
+                    ),
+                    Expanded(
+                        child: Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: TextField(
+                        controller: _baseAmountController,
+                        textAlign: TextAlign.end,
+                        keyboardType: TextInputType.numberWithOptions(
+                          signed: false,
+                          decimal: true,
+                        ),
+                        inputFormatters: [
+                          LengthLimitingTextInputFormatter(16),
+                          WhitelistingTextInputFormatter(
+                              RegExp("^[0-9]+[\.,]?[0-9]*\$"))
+                        ],
+                      ),
+                    )),
+                  ],
+                )),
           ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: _currencyRate.rates.length,
+                itemBuilder: (context, itemIndex) {
+                  final item = _currencyRate.rates[itemIndex];
+
+                  // TODO add fancier layout and formatting
+                  return Text("1.00 ${_currencyRate.baseCurrency} = ${item.rate} ${item.currency}");
+                }),
+          )
         ],
       ),
     );
