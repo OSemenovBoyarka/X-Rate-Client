922f27dad2e27c2eebba4ab27a39db9c585b8212
Alexander Semenov <alexs@2kgroup.com>
2019-05-03 11:21:55 +0300

    Added inherited widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,24 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class HomeStateInheritedWidget extends InheritedWidget {
+
+  final double baseAmount;
+
+  const HomeStateInheritedWidget({this.baseAmount, Widget child})
+      : super(child: child);
+
+  static HomeStateInheritedWidget of(BuildContext context) {
+    return context.inheritFromWidgetOfExactType(HomeStateInheritedWidget);
+  }
+
+  @override
+  bool updateShouldNotify(HomeStateInheritedWidget oldWidget) {
+    return baseAmount != oldWidget.baseAmount;
+  }
+}
+
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -31,9 +49,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    // TODO add async data init
     _currencyRateFuture = getRates();
-    // default amount is 1.00
     _currentBaseAmount = 1.00;
 
     // TODO add currency formatter
@@ -44,117 +60,123 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: FutureBuilder<CurrencyRate>(
-          future: _currencyRateFuture,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              // TODO retry button
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            }
+    return HomeStateInheritedWidget(
+      baseAmount: _currentBaseAmount,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: FutureBuilder<CurrencyRate>(
+            future: _currencyRateFuture,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                // TODO retry button
+                return Center(
+                  child: Text(snapshot.error.toString()),
+                );
+              }
 
-            // loading state should check waiting as well to cover all cases
-            if (!snapshot.hasData ||
-                snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+              // loading state should check waiting as well to cover all cases
+              if (!snapshot.hasData ||
+                  snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
 
-            var data = snapshot.data;
+              var data = snapshot.data;
 
-            List<Currency> availableCurrencies =
-            data.rates.map((rate) => rate.currency).toList();
-            // for some currencies backend return base currency in the list and for some - doesn't
-            if (!availableCurrencies.contains(data.baseCurrency)) {
-              availableCurrencies.add(data.baseCurrency);
-            }
+              List<Currency> availableCurrencies =
+              data.rates.map((rate) => rate.currency).toList();
+              // for some currencies backend return base currency in the list and for some - doesn't
+              if (!availableCurrencies.contains(data.baseCurrency)) {
+                availableCurrencies.add(data.baseCurrency);
+              }
 
-            return Column(
-              children: <Widget>[
-                Card(
-                  child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          DropdownButton<String>(
-                            value: data.baseCurrency.code,
-                            items: availableCurrencies
-                                .map((currency) =>
-                                DropdownMenuItem(
-                                  value: currency.code,
-                                  child: Text(currency.code),
-                                ))
-                                .toList(),
-                            onChanged: (String value) {
-                              // fetch new data from network
-                              setState(() {
-                                _currencyRateFuture =
-                                    getRates(baseCurrency: Currency(value));
-                              });
-                              print("Currency selected: $value");
-                            },
-                          ),
-                          Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 16.0),
-                                child: TextField(
-                                  controller: _baseAmountController,
-                                  textAlign: TextAlign.end,
-                                  keyboardType: TextInputType.numberWithOptions(
-                                    signed: false,
-                                    decimal: true,
+              return Column(
+                children: <Widget>[
+                  Card(
+                    child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            DropdownButton<String>(
+                              value: data.baseCurrency.code,
+                              items: availableCurrencies
+                                  .map((currency) =>
+                                  DropdownMenuItem(
+                                    value: currency.code,
+                                    child: Text(currency.code),
+                                  ))
+                                  .toList(),
+                              onChanged: (String value) {
+                                // fetch new data from network
+                                setState(() {
+                                  _currencyRateFuture =
+                                      getRates(baseCurrency: Currency(value));
+                                });
+                                print("Currency selected: $value");
+                              },
+                            ),
+                            Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 16.0),
+                                  child: TextField(
+                                    controller: _baseAmountController,
+                                    textAlign: TextAlign.end,
+                                    keyboardType: TextInputType
+                                        .numberWithOptions(
+                                      signed: false,
+                                      decimal: true,
+                                    ),
+                                    inputFormatters: [
+                                      LengthLimitingTextInputFormatter(16),
+                                      WhitelistingTextInputFormatter(
+                                          RegExp("^[0-9]+[\.,]?[0-9]*\$"))
+                                    ],
+                                    onChanged: (value) {
+                                      setState(() {
+                                        if (value.isEmpty) {
+                                          _currentBaseAmount = 0.0;
+                                        } else {
+                                          // TODO improve input formatting
+                                          _currentBaseAmount = double.tryParse(
+                                              value.replaceAll(",", "."));
+                                        }
+                                      });
+                                    },
                                   ),
-                                  inputFormatters: [
-                                    LengthLimitingTextInputFormatter(16),
-                                    WhitelistingTextInputFormatter(
-                                        RegExp("^[0-9]+[\.,]?[0-9]*\$"))
-                                  ],
-                                  onChanged: (value) {
-                                    setState(() {
-                                      if (value.isEmpty) {
-                                        _currentBaseAmount = 0.0;
-                                      } else {
-                                        // TODO improve input formatting
-                                        _currentBaseAmount = double.tryParse(
-                                            value.replaceAll(",", "."));
-                                      }
-                                    });
-                                  },
-                                ),
-                              )),
-                        ],
-                      )),
-                ),
-                Expanded(
-                  child: CurrencyRatesList(
-                    currencyRate: data,
-                    currentBaseAmount: _currentBaseAmount,
+                                )),
+                          ],
+                        )),
                   ),
-                ),
-              ],
-            );
-          }),
+                  Expanded(
+                    child: CurrencyRatesList(
+                        currencyRate: data
+                    ),
+                  ),
+                ],
+              );
+            }),
+      ),
     );
   }
 }
 
 class CurrencyRatesList extends StatelessWidget {
   final CurrencyRate currencyRate;
-  final double currentBaseAmount;
 
-  CurrencyRatesList({Key key, this.currencyRate, this.currentBaseAmount})
-      : super(key: key);
+  const CurrencyRatesList({Key key, this.currencyRate}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: currencyRate.rates.length,
         itemBuilder: (context, itemIndex) {
+          // get base amount from inherited widget
+          final baseAmount = HomeStateInheritedWidget
+              .of(context)
+              .baseAmount;
+
           final item = currencyRate.rates[itemIndex];
-          final convertedAmount = currentBaseAmount * item.rate;
+          final convertedAmount = baseAmount * item.rate;
           final converted = Money.fromDouble(convertedAmount, item.currency);
 
           return ListTile(
